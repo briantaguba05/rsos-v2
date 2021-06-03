568c977da63e650158f9b80b57e9bd1b0aaaf0d2
briantaguba05 <briantaguba05@gmail.com>
2021-06-03 15:19:03 +0800

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.css";
 import Home from "./pages";
 import SignInPage from "./pages/signin";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
